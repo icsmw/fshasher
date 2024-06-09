ea81c0515a04a0252e3f865f56f3ced05d1906a1
DmitryAstafyev <dvastafyev@gmail.com>
2024-06-09 20:42:24 +0200

    Add docs for pool and worker (walker)

--- src/walker/pool.rs
+++ src/walker/pool.rs
@@ -2,11 +2,35 @@ use super::{Action, ReadingStrategy, Worker};
 use crate::{breaker::Breaker, Hasher, Reader};
 use std::{slice::Iter, sync::mpsc::Sender};
 
+/// Created by the `Walker` function to manage available workers. Each worker takes a vector of file paths and manages the calculation of their hashes.
+/// To calculate a hash, the worker reads the file with the given reader and provides the file's content to the hasher, which returns the hash of the file.
+/// As soon as the worker completes all given paths, it returns a vector of file paths with hashes to the `Walker`.
 pub struct Pool<H: Hasher, R: Reader> {
     workers: Vec<Worker<H, R>>,
 }
 
 impl<H: Hasher + 'static, R: Reader + 'static> Pool<H, R> {
+    /// Creates a new `Pool` with the specified number of workers.
+    ///
+    /// # Parameters
+    ///
+    /// - `count`: The number of workers to create.
+    /// - `tx_queue`: The sender channel for sending actions to the workers.
+    /// - `reading_strategy`: The strategy for reading files.
+    ///   - `ReadingStrategy::Buffer` - Each file will be read in the "classic" way using a limited size buffer, chunk by
+    ///     chunk until the end. The hasher will receive small chunks of data to calculate the hash of the file. This strategy
+    ///     doesn't load the CPU much, but it entails many IO operations.
+    ///   - `ReadingStrategy::Complete` - With this strategy, the file will be read first and the complete file's content will
+    ///     be passed into the hasher to calculate the hash. This strategy makes fewer IO operations, but it loads the CPU more.
+    ///   - `ReadingStrategy::MemoryMapped` - Instead of reading the file, the reader tries to map the file into memory and give
+    ///     the full content of the file to the hasher.
+    ///   - `ReadingStrategy::Scenario(..)` - The scenario strategy can be used to combine different strategies according to the
+    ///     file's size.
+    /// - `breaker`: The breaker to handle interruptions.
+    ///
+    /// # Returns
+    ///
+    /// - A new `Pool` instance.
     pub fn new(
         count: usize,
         tx_queue: Sender<Action<H>>,
@@ -23,9 +47,21 @@ impl<H: Hasher + 'static, R: Reader + 'static> Pool<H, R> {
         }
         Self { workers }
     }
+
+    /// Returns an iterator over the workers.
+    ///
+    /// # Returns
+    ///
+    /// - `Iter<Worker<H, R>>`: An iterator over the workers.
     pub fn iter(&self) -> Iter<Worker<H, R>> {
         self.workers.iter()
     }
+
+    /// Checks if all workers are shut down.
+    ///
+    /// # Returns
+    ///
+    /// - `bool`: `true` if all workers are done, `false` otherwise.
     pub fn is_all_down(&self) -> bool {
         self.workers
             .iter()
@@ -33,6 +69,13 @@ impl<H: Hasher + 'static, R: Reader + 'static> Pool<H, R> {
             .sum::<usize>()
             == 0
     }
+
+    /// Sends a signal to each worker to shut down. This method doesn't wait for the workers to shut down;
+    /// it only sends a shutdown signal to the workers.
+    ///
+    /// # Returns
+    ///
+    /// - `&mut Self`: The modified `Pool` instance.
     pub fn shutdown(&mut self) -> &mut Self {
         for worker in self.workers.iter() {
             worker.shutdown();
@@ -40,6 +83,7 @@ impl<H: Hasher + 'static, R: Reader + 'static> Pool<H, R> {
         self
     }
 
+    /// Waits for all workers to shut down.
     pub fn wait(&mut self) {
         for worker in self.workers.iter_mut() {
             worker.wait();
--- src/walker/worker.rs
+++ src/walker/worker.rs
@@ -1,5 +1,4 @@
 use super::{Action, HasherWrapper, ReaderWrapper, ReadingStrategy, E};
-
 use crate::{breaker::Breaker, Hasher, Reader};
 use log::{debug, error};
 use std::{
@@ -17,13 +16,19 @@ const BUFFER_SIZE: usize = 1024 * 32;
 
 pub type Job<H, R> = (PathBuf, HasherWrapper<H>, ReaderWrapper<R>);
 
+/// Represents tasks for the `Worker` to perform.
 enum Task<H: Hasher, R: Reader> {
+    /// Task to hash a vector of jobs (file path, hasher, reader).
     Hash(Vec<Job<H, R>>),
+    /// Task to shut down the worker.
     Shutdown,
 }
 
 type TaskChannel<H, R> = (Sender<Task<H, R>>, Receiver<Task<H, R>>);
 
+/// `Worker` is used by `Walker` to read files and calculate hashes. `Worker` creates one thread and
+/// listens for incoming messages (`Task`). As a task, `Worker` receives a vector of files and assigns
+/// an instance of hasher and reader to each file.
 pub struct Worker<H: Hasher, R: Reader> {
     tx_task: Sender<Task<H, R>>,
     queue: Arc<RwLock<usize>>,
@@ -32,6 +37,23 @@ pub struct Worker<H: Hasher, R: Reader> {
 }
 
 impl<H: Hasher + 'static, R: Reader + 'static> Worker<H, R> {
+    /// Runs a new `Worker` instance.
+    ///
+    /// # Parameters
+    ///
+    /// - `tx_queue`: The sender channel for sending actions to the pool.
+    /// - `reading_strategy`: The strategy for reading files.
+    /// - `breaker`: The breaker to handle interruptions.
+    ///
+    /// # Returns
+    ///
+    /// - A new `Worker` instance.
+    ///
+    /// Errors handeling
+    ///
+    /// `Worker` doesn't stop the listener loop on IO errors (from hasher or reader), but reports the error
+    /// to `Walker`.
+
     pub fn run(
         tx_queue: Sender<Action<H>>,
         reading_strategy: ReadingStrategy,
@@ -47,7 +69,7 @@ impl<H: Hasher + 'static, R: Reader + 'static> Worker<H, R> {
                 *queue_inner.write().unwrap() -= 1;
                 tx_queue.send(action).map_err(|err| {
                     error!(
-                        "Worker cannot comunicate with pool. Channel error. Worker will be closed"
+                        "Worker cannot communicate with pool. Channel error. Worker will be closed"
                     );
                     err
                 })
@@ -79,9 +101,9 @@ impl<H: Hasher + 'static, R: Reader + 'static> Worker<H, R> {
             }
             available_inner.store(false, Ordering::Relaxed);
             if tx_queue.send(Action::WorkerShutdownNotification).is_err() {
-                error!("Worker cannot comunicate with pool. Channel error. Worker will be closed");
+                error!("Worker cannot communicate with pool. Channel error. Worker will be closed");
             }
-            debug!("Hasher worker has been shutdown");
+            debug!("Hasher worker has been shut down");
         });
         Self {
             tx_task,
@@ -91,25 +113,42 @@ impl<H: Hasher + 'static, R: Reader + 'static> Worker<H, R> {
         }
     }
 
+    /// Checks if the worker is free (i.e., has no tasks in the queue).
+    ///
+    /// # Returns
+    ///
+    /// - `bool`: `true` if the worker is free, `false` otherwise.
     pub fn is_free(&self) -> bool {
         *self.queue.read().unwrap() == 0
     }
 
+    /// Checks if the worker is available to take new tasks.
+    ///
+    /// # Returns
+    ///
+    /// - `bool`: `true` if the worker is available, `false` otherwise.
     pub fn is_available(&self) -> bool {
         self.available.load(Ordering::Relaxed)
     }
 
+    /// Delegates a task to read a vector of file paths to the worker.
+    ///
+    /// # Parameters
+    ///
+    /// - `jobs`: The vector of jobs (file paths, hasher, reader) to be processed by the worker.
     pub fn delegate(&self, jobs: Vec<Job<H, R>>) {
         *self.queue.write().unwrap() += 1;
         let _ = self.tx_task.send(Task::Hash(jobs));
     }
 
+    /// Sends a shutdown signal to the worker.
     pub fn shutdown(&self) {
         if self.is_available() {
             let _ = self.tx_task.send(Task::Shutdown);
         }
     }
 
+    /// Waits for the worker to shut down.
     pub fn wait(&mut self) {
         if let Some(handle) = self.handle.take() {
             let _ = handle.join();
@@ -117,6 +156,23 @@ impl<H: Hasher + 'static, R: Reader + 'static> Worker<H, R> {
     }
 }
 
+/// Hashes the content of a file based on the given reading strategy.
+///
+/// # Parameters
+///
+/// - `path`: The path of the file to be hashed.
+/// - `hasher`: The hasher instance to use for hashing the file.
+/// - `reader`: The reader instance to use for reading the file.
+/// - `reading_strategy`: The strategy to use for reading the file.
+/// - `breaker`: The breaker to handle interruptions.
+///
+/// # Returns
+///
+/// - `Result<HasherWrapper<H>, E>`: The resulting hasher instance with the file's hash or an error if the operation fails.
+///
+/// # Errors
+///
+/// This function will return an error if the operation is interrupted or if there is an issue with reading the file.
 fn hash_file<H: Hasher, R: Reader>(
     path: &Path,
     mut hasher: HasherWrapper<H>,
